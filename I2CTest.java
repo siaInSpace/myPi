a9116dd7a9f6d3b13230610a50be6975cd69999a
siaa20 <sia20@aber.ac.uk>
2018-05-27 21:29:10 +0200

    fix "Console"

--- I2CTest.java
+++ I2CTest.java
@@ -36,7 +36,6 @@ import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import com.pi4j.platform.PlatformAlreadyAssignedException;
-import com.pi4j.util.Console;
 
 /**
  * This example code demonstrates how to perform simple I2C communication on the
@@ -86,20 +85,15 @@ public class I2CTest {
 
         // create Pi4J console wrapper/helper
         // (This is a utility class to abstract some of the boilerplate code)
-        final Console console = new Console();
 
         // print program title/header
-        console.title("<-- The Pi4J Project -->", "I2C Example");
-
-        // allow for user to exit program using CTRL-C
-        console.promptForExit();
-
+        Syste.out.println("<-- The Pi4J Project -->", "I2C Example");
         // fetch all available busses
         try {
             int[] ids = I2CFactory.getBusIds();
-            console.println("Found follow I2C busses: " + Arrays.toString(ids));
+            Syste.out.println("Found follow I2C busses: " + Arrays.toString(ids));
         } catch (IOException exception) {
-            console.println("I/O error during fetch of I2C busses occurred");
+            Syste.out.println("I/O error during fetch of I2C busses occurred");
         }
 
         // find available busses
@@ -107,11 +101,11 @@ public class I2CTest {
             try {
                 @SuppressWarnings("unused")
                 I2CBus bus = I2CFactory.getInstance(number);
-                console.println("Supported I2C bus " + number + " found");
+                Syste.out.println("Supported I2C bus " + number + " found");
             } catch (IOException exception) {
-                console.println("I/O error on I2C bus " + number + " occurred");
+                Syste.out.println("I/O error on I2C bus " + number + " occurred");
             } catch (UnsupportedBusNumberException exception) {
-                console.println("Unsupported I2C bus " + number + " required");
+                Syste.out.println("Unsupported I2C bus " + number + " required");
             }
         }
 
@@ -127,13 +121,13 @@ public class I2CTest {
         // next, lets perform am I2C READ operation to the TSL2561 chip
         // we will read the 'ID' register from the chip to get its part number and
         // silicon revision number
-        console.println("... reading ID register from TSL2561");
+        Syste.out.println("... reading ID register from TSL2561");
         int response = device.read(TSL2561_REG_ID);
-        console.println("TSL2561 ID = " + String.format("0x%02x", response) + " (should be 0x50)");
+        Syste.out.println("TSL2561 ID = " + String.format("0x%02x", response) + " (should be 0x50)");
 
         // next we want to start taking light measurements, so we need to power up the
         // sensor
-        console.println("... powering up TSL2561");
+        Syste.out.println("... powering up TSL2561");
         device.write(TSL2561_REG_CONTROL, TSL2561_POWER_UP);
 
         // wait while the chip collects data
@@ -141,16 +135,16 @@ public class I2CTest {
 
         // now we will perform our first I2C READ operation to retrieve raw integration
         // results from DATA_0 and DATA_1 registers
-        console.println("... reading DATA registers from TSL2561");
+        Syste.out.println("... reading DATA registers from TSL2561");
         int data0 = device.read(TSL2561_REG_DATA_0);
         int data1 = device.read(TSL2561_REG_DATA_1);
 
         // print raw integration results from DATA_0 and DATA_1 registers
-        console.println("TSL2561 DATA 0 = " + String.format("0x%02x", data0));
-        console.println("TSL2561 DATA 1 = " + String.format("0x%02x", data1));
+        Syste.out.println("TSL2561 DATA 0 = " + String.format("0x%02x", data0));
+        Syste.out.println("TSL2561 DATA 1 = " + String.format("0x%02x", data1));
 
         // before we exit, lets not forget to power down light sensor
-        console.println("... powering down TSL2561");
+        Syste.out.println("... powering down TSL2561");
         device.write(TSL2561_REG_CONTROL, TSL2561_POWER_DOWN);
     }
 }
