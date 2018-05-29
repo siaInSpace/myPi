a181221b6e6b75a97fd08dd31db367ee3b77230f
siaa20 <sia20@aber.ac.uk>
2018-05-29 15:03:02 +0200

    removed "stirng to int"

--- myPi/src/main/java/sia20/myPi/App.java
+++ myPi/src/main/java/sia20/myPi/App.java
@@ -22,10 +22,10 @@ public class App {
 
     private void printCalVals(byte[][] vals, Word word) {
         for (int i = 0; i < 11; i++) {
-            System.out.print(String.format("%08d", Integer.toBinaryString(vals[i][0]&0xff)));
+            System.out.print(String.format("%08d", vals[i][0]&0xff));
             System.out.print("");
-            System.out.println(String.format("%08d", Integer.toBinaryString(vals[i][1]&0xff)));
-            System.out.println(String.format("%016L", Long.toBinaryString(word.combToLong(vals[i][0], vals[i][1]))));
+            System.out.println(String.format("%08d", vals[i][1]&0xff));
+            System.out.println(String.format("%016L", word.combToLong(vals[i][0], vals[i][1)));
         }
     }
 
