1df5587961bd6192aa56bc504cfcc85a02c4a2ec
siaa20 <sia20@aber.ac.uk>
2018-05-31 19:31:21 +0200

    reverses array to have bytes in correct places

--- myPi/src/main/java/sia20/myPi/Word.java
+++ myPi/src/main/java/sia20/myPi/Word.java
@@ -13,20 +13,20 @@ public class Word {
     }
 
     public static String byteToBinaryPadded(byte val) {
-        
-        //1010110011100 11110110
-        //0000000000000 11111111: 21, (21-8 = 13)-> 
-        //101011: 6, (6-8 = -2) ->
-        //0: 1, (1-8 = -7) ->
-        //10110011: 8 (8-8 = 0) ->
+
+        // 1010110011100 11110110
+        // 0000000000000 11111111: 21, (21-8 = 13)->
+        // 101011: 6, (6-8 = -2) ->
+        // 0: 1, (1-8 = -7) ->
+        // 10110011: 8 (8-8 = 0) ->
         int vals = val & 0xFF;
 
         String padded = Integer.toBinaryString(vals);
-        String fitSize =  padded.substring(padded.length()-8);
+        String fitSize = padded.substring(padded.length() - 8);
         for (int i = fitSize.length(); i < 8; i++) {
             padded = "0" + padded;
         }
-        return padded.substring(padded.length()-8) + "\n";
+        return padded.substring(padded.length() - 8) + "\n";
     }
 
     public byte[] readBytes(int regAddr, int nrAddr) {
@@ -64,16 +64,24 @@ public class Word {
         res = (res >> (8 - oss));
         return res;
     }
-    
-    public short toShort(byte[] vals){
-        return (short)(BitSet.valueOf(vals).toLongArray()[0]);
+
+    private byte[] reverseArray(byte[] arr) {
+        byte[] reversed = new byte[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - (i + 1)];
+        }
+        return reversed;
     }
 
-    public int toInt(byte[] vals){
-        return (int)(BitSet.valueOf(vals).toLongArray()[0]);
+    public short toShort(byte[] vals) {
+        return (short) (BitSet.valueOf(reverseArray(vals)).toLongArray()[0]);
     }
-    
-    public long toLong(byte[] vals){
-        return (BitSet.valueOf(vals).toLongArray()[0]);
+
+    public int toInt(byte[] vals) {
+        return (int) (BitSet.valueOf(reverseArray(vals)).toLongArray()[0]);
+    }
+
+    public long toLong(byte[] vals) {
+        return (BitSet.valueOf(reverseArray(vals)).toLongArray()[0]);
     }
 }
