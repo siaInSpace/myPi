b9d367d58838c45a03a85798144f4df2680b450b
siaa20 <sia20@aber.ac.uk>
2018-06-01 22:49:41 +0200

    Trying to compile all classes at once

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
-javac ./myPi/src/main/java/sia20/myPi/App.java -d ./myPi/target/classes
+javac ./myPi/src/main/java/sia20/myPi/* -d ./myPi/target/classes
 java -cp /opt/pi4j/lib/*:./myPi/target/classes/ sia20.myPi.App
