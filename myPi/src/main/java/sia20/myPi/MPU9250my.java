34d88b938c34074b2b293c9ff52d61f1d124557e
siaa20 <sia20@aber.ac.uk>
2018-06-02 20:41:15 +0200

    Cleaned up class to prepare for proper implementations

--- myPi/src/main/java/sia20/myPi/MPU9250my.java
+++ myPi/src/main/java/sia20/myPi/MPU9250my.java
@@ -6,22 +6,13 @@ import com.pi4j.io.i2c.I2CFactory;
 
 import java.io.IOException;
 
-public class MPU9250my {
+class MPU9250my {
     private I2CDevice device;
     private int sensAddr = 0x68;
-    private final int startAddr = 0x3B;
-    private final int startAddrMag = 0x03;
     private final int whoAmIAddr = 0x75;
     private final int whoAmIValueDefualt = 0x73;// usually 0x71, i don't know why this is 0x73 instead
-    // register addresses for 9250 first address is 0x3B all others is pos(in data)
-    // + this address(0x3B)//
-    public final String[] data = { "accXH", "accXL", "accYH", "accYL", "accZH", "accZL", "tempH", "tempL", "gyrXH",
-            "gyrXL", "gyrYH", "gyrYL", "gyrZH", "gyrZL" };
-    // register addresses for the magnetometer first address is 0x03 all others is
-    // pos(in data) + this address(0x0B)//
-    public final String[] dataMag = { "magXL", "magXH", "magYL", "magYH", "magZL", "magZH" };
 
-    public MPU9250my() {
+    MPU9250my() {
         I2CBus bus = null;
         try {
             bus = I2CFactory.getInstance(I2CBus.BUS_1);
@@ -37,30 +28,10 @@ public class MPU9250my {
         }
     }
 
-    private long[] readData() throws IOException {
-        long[] res = new long[data.length];
-        for (int i = 0; i < data.length; i++) {
-            res[i] = device.read(startAddr + i);
-        }
-        return res;
-    }
 
-    private long[] readMag() throws IOException {
-        long[] res = new long[dataMag.length];
-        for (int i = 0; i < dataMag.length; i++) {
-            res[i] = device.read(startAddrMag + i);
-        }
-        return res;
-    }
 
-    public long[][] readAll() throws IOException {
-        long[][] res = new long[2][];
-        res[0] = readData();
-        res[1] = readMag();
-        return res;
-    }
 
-    public void whoAmI() {
+    void whoAmI() {
         int res = 0;
         try {
             res = device.read(whoAmIAddr);
