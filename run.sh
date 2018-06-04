e752d17311d4c6a61076851ada888a88b736a395
siaa20 <37308360+siaa20@users.noreply.github.com>
2018-06-04 16:50:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+
 #!/bin/bash
+rm -rf ./myPi/target
+rm -rf ./data
 git pull
-javac ./myPi/src/main/java/sia20/myPi/* -d ./myPi/target/classes
+mkdir -p myPi/target/classes
+mkdir -p ./data/measurements
+mkdir -p ./data/CalibrationValues
+javac -d "./myPi/target/classes" -cp "/opt/pi4j/lib/*" ./myPi/src/main/java/sia20/myPi/*
 java -cp /opt/pi4j/lib/*:./myPi/target/classes/ sia20.myPi.App
